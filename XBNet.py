8f7f9cf5d7722c0fd798430fcc96681fd0a9ff52
Tianqi Guo <tqguo246@gmail.com>
2021-09-25 21:43:46 -0700

    training script

---
+++ XBNet.py
@@ -0,0 +1,195 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+N_GROUP = 8
+
+class ASPP(nn.Module):
+    def __init__(self, inplanes, outplanes):
+        super(ASPP, self).__init__()   
+        ASPP_planes = inplanes//2
+        self.aspp_paths = nn.ModuleList()
+        kernel_sizes = [1, 3, 3, 3]
+        dilations = [1, 6, 12, 18]
+        paddings = [0] + dilations[1:]
+        for i in range(4):
+            self.aspp_paths.append(                
+                nn.Sequential(
+                    nn.Conv2d(inplanes, ASPP_planes, kernel_size=kernel_sizes[i], 
+                              stride=1, padding=paddings[i], dilation=dilations[i], bias=False),
+                    nn.GroupNorm(N_GROUP, ASPP_planes),
+                    nn.ReLU(inplace=True)
+                )
+            )
+        
+        self.aspp_paths.append(
+            nn.Sequential(
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Conv2d(inplanes, ASPP_planes, 1, stride=1, bias=False),
+                nn.GroupNorm(N_GROUP, ASPP_planes),
+                nn.ReLU(inplace=True)
+            )  
+        )
+        
+        self.exit = nn.Sequential(
+            nn.Conv2d(ASPP_planes*5, outplanes, 1, bias=False),
+            nn.GroupNorm(N_GROUP, outplanes)
+        )
+        if inplanes != outplanes:
+            self.skip = nn.Sequential(
+                nn.Conv2d(inplanes, outplanes, kernel_size=1, bias=False),  
+                nn.GroupNorm(N_GROUP, outplanes)
+            )     
+        else:
+            self.skip = nn.Sequential()            
+        
+    def forward(self, x): 
+        skip = self.skip(x) 
+        xs = [path(x) for path in self.aspp_paths]
+        xs[-1] = F.interpolate(xs[-1], size=x.shape[-2:], mode='bilinear', align_corners=True)             
+        xs = torch.cat(xs, dim=1)        
+        out = self.exit(xs)  
+        out = out + skip
+        return F.relu(out, inplace=True) 
+    
+class UpBlock(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(UpBlock, self).__init__()
+        self.up = nn.Sequential(
+            nn.PixelShuffle(upscale_factor=2),
+            nn.GroupNorm(N_GROUP, out_size//2),
+            nn.ReLU(inplace=True)
+        )
+        self.bridge = nn.Sequential(
+            nn.Conv2d(out_size, out_size//2, kernel_size=1, bias=False),  
+            nn.GroupNorm(N_GROUP, out_size//2),
+            nn.ReLU(inplace=True)
+        )
+ 
+        nf = out_size
+        gc = out_size//4
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(nf, gc, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, gc),
+            nn.ReLU(inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(nf + gc, gc, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, gc),
+            nn.ReLU(inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(nf + 2 * gc, gc, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, gc),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(nf + 3 * gc, gc, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, gc),
+            nn.ReLU(inplace=True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(nf + 4 * gc, nf, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, nf),            
+        )       
+        
+    def forward(self, x, bridge):        
+        up = self.up(x)  
+        bridge = self.bridge(bridge)
+        x0 = torch.cat([up, bridge], 1)        
+        
+        x1 = self.conv1(x0)
+        x2 = self.conv2(torch.cat((x0, x1), 1))
+        x3 = self.conv3(torch.cat((x0, x1, x2), 1))
+        x4 = self.conv4(torch.cat((x0, x1, x2, x3), 1))
+        x5 = self.conv5(torch.cat((x0, x1, x2, x3, x4), 1))
+        x6 = x5 * 0.2 + x0
+        return F.relu(x6, inplace=True)         
+    
+class DownBlock(nn.Module):
+    def __init__(self, inplanes, planes):
+        super(DownBlock, self).__init__() 
+        
+        nf = inplanes
+        gc = inplanes//4
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(nf, gc, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, gc),
+            nn.ReLU(inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(nf + gc, gc, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, gc),
+            nn.ReLU(inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(nf + 2 * gc, gc, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, gc),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(nf + 3 * gc, gc, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, gc),
+            nn.ReLU(inplace=True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(nf + 4 * gc, nf, 3, 1, 1, bias=False),
+            nn.GroupNorm(N_GROUP, nf)
+        )          
+        
+        self.down = nn.Sequential( 
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inplanes, planes, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.GroupNorm(N_GROUP, planes),
+            nn.ReLU(inplace=True)
+        )                    
+
+    def forward(self, x):
+                
+        x1 = self.conv1(x)
+        x2 = self.conv2(torch.cat((x, x1), 1))
+        x3 = self.conv3(torch.cat((x, x1, x2), 1))
+        x4 = self.conv4(torch.cat((x, x1, x2, x3), 1))
+        x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
+        x6 = x5 * 0.2 + x
+        
+        return self.down(x6)
+
+class XBNet(nn.Module):    
+    def __init__(self):
+        super(XBNet, self).__init__()        
+        chns = 32        
+        self.entry_flow = nn.Sequential(   
+            nn.Conv2d(1, chns, 7, stride=1, padding=3, bias=False),
+            nn.GroupNorm(N_GROUP, chns),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(chns, chns, 3, stride=1, padding=1, bias=False),
+            nn.GroupNorm(N_GROUP, chns),
+            nn.ReLU(inplace=True)
+        )         
+        self.down_path = nn.ModuleList()
+        for _ in range(4):
+            self.down_path.append(DownBlock(chns, chns*2))  
+            chns *= 2
+        self.ASPP = ASPP(chns, chns)         
+        self.up_path = nn.ModuleList()
+        for _ in range(4):
+            self.up_path.append(UpBlock(chns, chns//2))
+            chns //= 2
+        self.last = nn.Conv2d(chns, 3, kernel_size=1)
+        
+    def forward(self, x):        
+        feature_maps = []        
+        x = self.entry_flow(x)
+        feature_maps += [x]         
+        for i, down in enumerate(self.down_path):
+            x = down(x) 
+            if i < 3:                
+                feature_maps += [x]                       
+        x = self.ASPP(x)           
+        for up, feature_map in zip(self.up_path, feature_maps[::-1]):   
+            x = up(x, feature_map)        
+        return self.last(x)  
---
+++ defs.py
@@ -0,0 +1,189 @@
+import os
+import math
+import glob
+import shutil
+import random
+import imageio
+
+import tracemalloc
+import numpy as np
+
+from scipy.ndimage import measurements
+
+import torch
+import torch.utils.data as data_utils
+from torch.utils.data import DataLoader
+
+import shlex
+from datetime import datetime
+from subprocess import Popen, PIPE
+
+class InfiniteDataLoader(DataLoader):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Initialize an iterator over the dataset.
+        self.dataset_iterator = super().__iter__()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            batch = next(self.dataset_iterator)
+        except StopIteration:
+            # Dataset exhausted, use a new fresh iterator.
+            self.dataset_iterator = super().__iter__()
+            batch = next(self.dataset_iterator)
+        return batch
+
+def dimension_matching(cell_pred, y):
+    
+    _, h, w = y.shape
+    _, H, W = cell_pred.shape
+    
+    if H > h:
+        pad_H_0 = (H - h)//2
+        pad_H_1 = H - h - pad_H_0
+        pad_W_0 = (W - w)//2
+        pad_W_1 = W - w - pad_W_0
+        cell_padded = cell_pred[:,pad_H_0:-pad_H_1,pad_W_0:-pad_W_1]
+    elif H < h:
+        pad_H_0 = (h - H)//2
+        pad_H_1 = h - H - pad_H_0
+        pad_W_0 = (w - W)//2
+        pad_W_1 = w - W - pad_W_0
+        cell_padded = np.pad(cell_pred, ((0,0),(pad_H_0,pad_H_1), (pad_W_0,pad_W_1)), 'constant', constant_values=0)
+    
+    return cell_padded
+
+@torch.no_grad()
+def eval_performance_test(model, dataset_loader, device, result_dir, set_id):
+    
+    case_dir = os.path.join(result_dir)
+    
+    RS_dir = os.path.join(case_dir,set_id+'_RES')
+    
+    if os.path.exists(RS_dir):
+        shutil.rmtree(RS_dir)
+    os.makedirs(RS_dir,exist_ok=True)
+    
+    counter = 0
+    
+    for X, y in dataset_loader:
+        
+        pred = model(X.to(device))
+        prediction = pred.cpu().detach().numpy()  # [N, 2, H, W]   
+        cell_pred = np.logical_and(prediction[:,0,:,:] < prediction[:,2,:,:],prediction[:,1,:,:] < prediction[:,2,:,:])
+        cell_padded = dimension_matching(cell_pred, y)
+        
+        for cell_padded_i in cell_padded:
+            if 'Fluo-C3DH-A549' in result_dir:
+                seg_RS_i = cell_padded_i
+            else:
+                seg_RS_i, _ = measurements.label(cell_padded_i)
+            imageio.imwrite(os.path.join(RS_dir,'mask%0.3d.tif'%counter), np.asarray(seg_RS_i,dtype=np.uint16))
+            counter += 1
+    
+    cmd = "./eval/Linux/SEGMeasure "+case_dir+" "+set_id+" 3"
+    process = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+    out, err = process.communicate()
+    
+    #print(cmd, out, err)
+    
+    exitcode = process.returncode
+    s = str(out, 'utf-8')
+    
+    file = open(os.path.join(RS_dir,'SEG_log.txt'), 'r') 
+    Lines = file.readlines() 
+      
+    scores = []
+    for line in Lines: 
+        if 'J' in line:
+            s = line.strip().split()[1]
+            score = float(''.join([c for c in s if c == '.' or c.isdigit()]))
+            scores.append(score)
+    file.close() 
+    
+    det_score = np.count_nonzero(scores)/len(scores)
+    seg_score = np.mean(scores)
+    #float(''.join([c for c in s if c == '.' or c.isdigit()]))
+    return(det_score, seg_score, scores)
+    
+def show_usage():
+    
+    print('')
+    current, peak = tracemalloc.get_traced_memory()
+    print("Current RAM usage is %07.2f MB; Peak was %07.2f MB"%(current/10**6,peak/10**6))
+    current, peak = torch.cuda.memory_allocated(), torch.cuda.max_memory_allocated()
+    print("Current GPU usage is %07.2f MB; Peak was %07.2f MB"%(current/10**6,peak/10**6))
+    print('')
+    
+class LoadDataset(data_utils.Dataset):    
+
+    def __init__(self, data_path, crop_win, data_aug, pad):
+        self.dataset_list = sorted(glob.glob(os.path.join(data_path,'*.npy')))        
+        self.crop_win = crop_win        
+        self.data_aug = data_aug
+        self.pad = pad   
+        
+    def __getitem__(self, index):
+        img, lbl = np.load(self.dataset_list[index],allow_pickle=True)
+        
+        if (self.crop_win == 0):
+            img_aug = img
+            lbl_aug = lbl 
+        else:
+            _, r, c = img.shape     
+            max_r = r - self.crop_win
+            max_c = c - self.crop_win
+            #print(img.shape,self.crop_win)
+            
+            r1 = random.randint(0, max_r)
+            r2 = r1 + self.crop_win
+            c1 = random.randint(0, max_c)
+            c2 = c1 + self.crop_win
+            
+            img_cropped = img[:,r1:r2,c1:c2]
+            lbl_cropped = lbl[r1:r2,c1:c2]                       
+        
+            if (self.data_aug == 0):
+                img_aug = img_cropped
+                lbl_aug = lbl_cropped    
+            else:
+                aug_type = random.randrange(6)
+                if aug_type == 0:
+                    img_aug = img_cropped
+                    lbl_aug = lbl_cropped              
+                elif aug_type == 1:
+                    img_aug = np.flip(img_cropped,-1)
+                    lbl_aug = np.flip(lbl_cropped,-1)
+                elif aug_type == 2:
+                    img_aug = np.flip(img_cropped,-2)
+                    lbl_aug = np.flip(lbl_cropped,-2)
+                elif aug_type == 3:
+                    img_aug = np.rot90(img_cropped,1,(-1,-2))
+                    lbl_aug = np.rot90(lbl_cropped,1,(-1,-2))
+                elif aug_type == 4:
+                    img_aug = np.rot90(img_cropped,2,(-1,-2))
+                    lbl_aug = np.rot90(lbl_cropped,2,(-1,-2))
+                elif aug_type == 5:
+                    img_aug = np.rot90(img_cropped,3,(-1,-2))
+                    lbl_aug = np.rot90(lbl_cropped,3,(-1,-2))             
+        
+        if (self.pad != None):
+            img_aug = np.pad(img_aug,((0,0), (self.pad[0],self.pad[1]),(self.pad[2],self.pad[3])),mode='symmetric')
+        else:
+            _, h, w = img_aug.shape   
+            H = (math.ceil(h/16)+1) * 16
+            W = (math.ceil(w/16)+1) * 16
+            pad_H_0 = (H - h)//2
+            pad_H_1 = H - h - pad_H_0
+            pad_W_0 = (W - w)//2
+            pad_W_1 = W - w - pad_W_0
+            img_aug = np.pad(img_aug,((0,0), (pad_H_0,pad_H_1),(pad_W_0,pad_W_1)),mode='symmetric')
+            
+        #print(type(img_aug), type(lbl_aug))
+        return tuple([img_aug.copy(), lbl_aug.copy()]) 
+
+    def __len__(self):
+        return len(self.dataset_list)
